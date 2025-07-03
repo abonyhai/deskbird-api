c1d153c4b1fe6bbea679cbf125f7a248ac5e6908
Andrei Bonyhai <andrei.bonyhai@atta.systems>
2025-07-03 12:57:11 +0300

    user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule, TypeOrmConfig } from './shared/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './resources/user/user.module';
 
 @Module({
-  imports: [AppConfigModule, TypeOrmModule.forRootAsync(TypeOrmConfig)],
+  imports: [
+    AppConfigModule,
+    TypeOrmModule.forRootAsync(TypeOrmConfig),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/resources/user/user.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ApiProjectController } from 'src/shared/decorators/controller';
+import { ApiProjectRoute } from 'src/shared/decorators/method';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@ApiProjectController('users')
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @ApiProjectRoute({
+    path: '/',
+    method: 'GET',
+    ok: { description: 'List all users' },
+  })
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @ApiProjectRoute({
+    path: '/:id',
+    method: 'GET',
+    ok: { description: 'Get user by id' },
+  })
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @ApiProjectRoute({
+    path: '/',
+    method: 'POST',
+    ok: { description: 'Create user' },
+  })
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @ApiProjectRoute({
+    path: '/:id',
+    method: 'PATCH',
+    ok: { description: 'Update user' },
+  })
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() dto: UpdateUserDto) {
+    return this.userService.update(id, dto);
+  }
+
+  @ApiProjectRoute({
+    path: '/:id',
+    method: 'DELETE',
+    ok: { description: 'Delete user' },
+  })
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.userService.remove(id);
+  }
+}
