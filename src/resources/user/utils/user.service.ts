4311c69acab7b30a3e384b6d661a20e1d9c4c99a
Andrei Bonyhai <andrei.bonyhai@atta.systems>
2025-07-03 13:57:31 +0300

    update structure

---
+++ src/resources/user/utils/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../types/user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  create(dto: CreateUserDto) {
+    const user = this.userRepository.create(dto);
+    return this.userRepository.save(user);
+  }
+
+  update(id: number, dto: UpdateUserDto) {
+    return this.userRepository.update(id, dto);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
