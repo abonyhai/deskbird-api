e896a36f14c7cc61eca01c3bc8a144e051d30037
Andrei Bonyhai <andrei.bonyhai@atta.systems>
2025-07-03 10:35:06 +0300

    move config to shared/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule, TypeOrmConfig } from './shared/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get('DB_PORT'), 10) || 5432,
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true, // Set to false in production
-      }),
-    }),
-  ],
+  imports: [AppConfigModule, TypeOrmModule.forRootAsync(TypeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/config/index.ts
@@ -0,0 +1,19 @@
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const AppConfigModule = ConfigModule.forRoot({ isGlobal: true });
+
+export const TypeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    host: config.get('DB_HOST'),
+    port: parseInt(config.get('DB_PORT'), 10) || 5432,
+    username: config.get('DB_USERNAME'),
+    password: config.get('DB_PASSWORD'),
+    database: config.get('DB_DATABASE'),
+    autoLoadEntities: true,
+    synchronize: true, // Set to false in production
+  }),
+}; 
